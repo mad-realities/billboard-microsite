46cc439ae41c453cf625c4327639593e4309ba59
David Promisel <dpromisel@me.com>
2022-11-29 18:37:46 -0500

    vote endpoint

---
+++ src/pages/api/vote.ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "POST") {
+    res.setHeader("Allow", ["POT"]);
+    res.status(405).end("Method Not Allowed");
+  } else {
+    try {
+      const { communityId, instagramHandle, apiSecret } = req.body;
+      if (apiSecret !== process.env.API_SECRET) {
+        res.status(401).end("Unauthorized");
+      } else if (!communityId || !instagramHandle) {
+        res.status(400).end("Bad Request, missing communityId or instagramHandle");
+      } else {
+        const vote = await prisma.vote.create({
+          data: {
+            communityId,
+            instagramHandle,
+          },
+        });
+        res.status(200).json(vote);
+      }
+    } catch {
+      res.status(500).end("Server Error");
+    }
+  }
+};
+
+export default handler;
