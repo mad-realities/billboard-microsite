8bf5aaf952309180f2bcb81367bbd0106ad713ea
Alice Ma <alicema@berkeley.edu>
2022-11-30 16:03:32 -0500

    add lilitaOne

--- src/components/design-system/BillboardButton.tsx
+++ src/components/design-system/BillboardButton.tsx
@@ -2,7 +2,8 @@ import Button, { ButtonProps } from "./Button";
 
 export default function BillboardButton(props: ButtonProps) {
   return (
-    <Button {...props} className="rounded-3xl border-4 border-double border-black ">
+    // outline-offset-4
+    <Button {...props} className="rounded-[10px] border-4 border-double border-black font-mono">
       {props.children}
     </Button>
   );
