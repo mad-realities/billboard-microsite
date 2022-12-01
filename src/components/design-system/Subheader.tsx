cc75d47a519d1526044f9a80487672fa5a598c82
Alice Ma <alicema@berkeley.edu>
2022-12-01 16:03:05 -0500

    cover

--- src/components/design-system/Subheader.tsx
+++ src/components/design-system/Subheader.tsx
@@ -12,7 +12,7 @@ export default function Subheader({ children, flipped }: SubheaderProps) {
         "w-full",
         "flex-col",
         "bg-[url('/subheader.png')]",
-        "bg-contain",
+        "bg-cover",
         "bg-no-repeat",
         "min-h-[24px]",
         "bg-bottom",
