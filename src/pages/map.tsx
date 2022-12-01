c570f768805e6fc0bffa933a8b6bcea0a226cdb3
Shrey Gupta <shreygupta97@gmail.com>
2022-12-02 04:35:06 +0800

    more lint happiness

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Map, Marker } from "pigeon-maps";
 import { stamenToner } from "pigeon-maps/providers";
 
-const billboards = [
-  [40.719873, -74.001863],
-  [40.6892, 74.0445],
-  [40.719874, -74.001862],
-];
+// const billboards = [
+//   [40.719873, -74.001863],
+//   [40.6892, 74.0445],
+//   [40.719874, -74.001862],
+// ];
 
 export default function MyMap() {
-  const [center, _] = React.useState([40.719873, -74.001863]);
+  const [center] = React.useState([40.719873, -74.001863]);
   // const markers = billboards.map((billboard, i) => (
   //   <Marker key={i} anchor={[billboard[0], billboard[1]]} color="red" onClick={() => console.log(i, billboard)} />
   // ));
