bee052bdfb6146a8dcc0e63a32c6c60fa51068b6
David Promisel <dpromisel@me.com>
2022-12-05 16:42:12 -0500

    link preview

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,18 +9,27 @@ import { useRouter } from "next/router";
 function BillboardApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
-  return (
-    <div className="flex min-h-screen flex-col bg-star-texture bg-contain">
-      <Head>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <title>MAD FAMOUS by Mad Realities</title>
-      </Head>
-      {router.pathname.includes("preview") ? (
+  if (router.pathname.includes("preview")) {
+    return (
+      <div className="flex flex-col bg-star-texture bg-contain">
+        <Head>
+          <title>MAD FAMOUS: THE BILLBOARD</title>
+          <meta name="description" content="MAD FAMOUS: THE BILLBOARD" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         <div className="align-center flex w-full flex-col items-center text-mr-offwhite">
           <Component {...pageProps} />
         </div>
-      ) : (
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex min-h-screen flex-col bg-star-texture bg-contain">
+        <Head>
+          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <title>MAD FAMOUS by Mad Realities</title>
+        </Head>
         <div className="mx-auto my-2 w-full max-w-md text-mr-offwhite">
           <NavBar />
           <div className="flex h-full w-full px-2 ">
@@ -32,8 +41,8 @@ function BillboardApp({ Component, pageProps }: AppProps) {
             </div>
           </div>
         </div>
-      )}
-    </div>
-  );
+      </div>
+    );
+  }
 }
 export default BillboardApp;
