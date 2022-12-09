2cb48bd1411bf737a519fa7f0c8383dab5aca299
Shrey Gupta <shreygupta97@gmail.com>
2022-12-09 05:33:53 -0500

    Add CSP

--- next.config.js
+++ next.config.js
@@ -1,7 +1,35 @@
 /**
  * @type {import('next').NextConfig}
  * */
+
+const contentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  style-src 'self';
+  font-src 'self';
+  frame-src 'self';
+  child-src 'self';
+  img-src 'self';
+  media-src 'self';
+`;
+
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy-Report-Only",
+    value: contentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  },
+];
+
 module.exports = {
   poweredByHeader: false,
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
